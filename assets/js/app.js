02abde9b5f580d3022d136dea48ba19a36a95a49
Erick Cepeda <ecepedavelazquez@gmail.com>
2022-10-28 22:29:40 -0500

    feat: subir codigo

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const $n = document.querySelector('#name');
+const $b = document.querySelector('#blog');
+const $l = document.querySelector('#location');
+
+async function displayUser(username) {
+  try {
+  $n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();
+  console.log(data);
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
+}
+  catch{
+    
+  }
+}
+function handleError(err) {
+  console.log('OH NO!');
+  console.log(err);
+  $n.textContent = `Algo salió mal: ${err}`
+}
+
+displayUser('stolinski').catch(handleError);
